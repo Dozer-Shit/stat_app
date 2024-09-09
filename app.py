3627e29e2adc4bf9157ca80ffc4728c408cf5072
Dozer <shitov.ea@gmail.com>
2024-09-09 06:21:43 +0400

    del idea

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
 import requests
+import threading
+import time
 
 app = Flask(__name__)
+
+site_status = {"status": "UNKNOWN", "code": None}
 
 
 def check_site_status(url):
@@ -18,6 +22,15 @@ def check_site_status(url):
         return {"status": "ERROR", "message": str(e)}
 
 
+def periodic_check():
+    site = "https://okulik.by"
+    while True:
+        global site_status
+        site_status = check_site_status(site)
+        print(f"Checked site status: {site_status}")  # Это выводится в консоль для отладки
+        time.sleep(5)
+
+
 @app.route('/')
 def index():
     return "Welcome to the status check application!"
@@ -25,10 +38,12 @@ def index():
 
 @app.route('/status')
 def status():
-    site = "https://okulik.by"
-    result = check_site_status(site)
-    return jsonify(result)
+    return jsonify(site_status)
 
 
 if __name__ == "__main__":
+    checker_thread = threading.Thread(target=periodic_check)
+    checker_thread.daemon = True
+    checker_thread.start()
+
     app.run(host="0.0.0.0", port=8080)
